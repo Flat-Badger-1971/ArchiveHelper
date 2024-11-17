31a8adaa5b7f9e4aeb4f91d247b6e0f777848daa
Anthony Oakley <agoakley@gmail.com>
2024-11-17 11:55:52 +0000

    version

--- settings/settings.lua
+++ settings/settings.lua
@@ -7,7 +7,7 @@ local panel = {
     name = "Archive Helper",
     displayName = zo_iconFormat("/esoui/art/icons/poi/poi_endlessdungeon_complete.dds") .. " |cff9900Archive Helper|r",
     author = "Flat Badger",
-    version = "1.3.6",
+    version = AH.LC.GetAddonVersion(AH.Name),
     registerForRefresh = true
 }
 local favouriteChoices = {}
